7f6d9d4a6b50542b0060fd39f6c3bffc49b39d54
bishoe01 <bishoe01@gmail.com>
2022-05-09 16:19:59 +0900

    9753 Collections solved

--- INT/9753.py
+++ INT/9753.py
@@ -1,10 +1,18 @@
+from collections import Counter ##key의 개수를 알기위한 Counter라이브러리 임포트
 N = int(input())
+
 
 for i in range(N):
     testcase = int(input())
-    dict ={}
+    arr =[]
     for _ in range(testcase):
-        category, cloth = map(str, input().split())
-        dict[category] = cloth
-        
-print(dict)
+        cloth,category = map(str, input().split())
+        arr.append(category) ##옷이 무엇인지는 계산할 떄 필요하지 않으므로 category만 리스트에 추가를해준다. 
+    
+    sum = 1 
+    result = Counter(arr)  #Counter로 옷 종류(Key)가 몇 종류 있는지 딕셔너리로 만들어준다.
+    for key in result :  #이때 key의 값은 
+        sum *=result[key] + 1
+    print(sum-1)
+    
+    
