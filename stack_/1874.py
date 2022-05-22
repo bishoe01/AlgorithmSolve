60573903830b6df7387f61835ad660c117367a7e
bishoe01 <bishoe01@gmail.com>
2022-05-22 13:56:39 +0900

    add annotation for 1874.py

--- stack_/1874.py
+++ stack_/1874.py
@@ -1,21 +1,21 @@
 n = int(input())
-stack , sign, find = [], [] , True
-std = 1
 
+stack , sign , std =[] , [] , 1
+result = True
 for _ in range(n):
-    target = int(input())
+    target = int(input()) #목표로 하는 숫자
     while(std <= target):
         stack.append(std)
         sign.append('+')
-        std += 1
+        std+=1
     
-    if(stack[-1]== target):
+    if(stack[-1] == target): #목표 값까지 스택에 채워줬을때는 이제 마지막 요소를 삭제해준다 (pop한 것은 상상 속에 스택에 저장한다고 생각)
         stack.pop()
         sign.append('-')
     else:
-        find= False
-if not find :
+        result = False
+if(result == False):
     print("NO")
 else:
-    for i in sign:
-        print(i)
+    for element in sign:
+        print(element)
