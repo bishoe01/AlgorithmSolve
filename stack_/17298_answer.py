483e1d71c0ce14eb82b56372dc807513293c9ebd
bishoe01 <bishoe01@gmail.com>
2022-05-23 00:44:19 +0900

    error fix

--- stack_/17298_answer.py
+++ stack_/17298_answer.py
@@ -1,30 +1,10 @@
 n = int(input())
 arr = list(map(int, input().split()))
-
 result = [-1]*n
 stack = []
-
 for i in range(n):
     while(len(stack) != 0 and arr[stack[-1]] < arr[i]):
         result[stack[-1]] = arr[i]
         stack.pop()
     stack.append(i)
 print(*result)
-
-
-
-
-n = int(input())
-arr = list(map(int, input().split()))
-
-result = [-1]*n
-stack = []
-
-for i in range(n):
-    while(len(stack) != 0 and arr[stack[-1]]< arr[i]):
-        result[stack[-1]] = arr[stack[-1]]
-        stack.pop()
-    stack.append(i)
-
-for element in result:
-    print(element,end=" ")
