207532733bd677ed61180279a6b5fe6e1a288d29
bishoe01 <bishoe01@gmail.com>
2022-05-22 23:59:43 +0900

    17298py solve

---
+++ stack_/17298.py
@@ -0,0 +1,23 @@
+n = int(input())
+arr = list(map(int,input().split()))
+tmp = 0;
+result = []
+
+
+for i in range(n):
+    if(i == n-1 ):
+        result.append(-1)
+        break
+    for k in range(i+1,n):
+        if(arr[i] < arr[k]):
+            tmp = arr[k]
+            break
+        else:
+            continue
+    if(tmp != 0):
+        result.append(tmp)
+    else:
+        result.append(-1)
+
+for element in result:
+    print(element,end=" ")
