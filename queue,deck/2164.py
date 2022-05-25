808e8d606001688c63b4af67e565509b0aa0715b
bishoe01 <bishoe01@gmail.com>
2022-05-25 22:33:39 +0900

    2164 card2

---
+++ queue,deck/2164.py
@@ -0,0 +1,10 @@
+from collections import deque
+n  =int(input())
+arr = deque([])
+for i in range(n):
+    arr.append(i+1)
+
+while(len(arr) > 1):
+    arr.popleft()
+    arr.append(arr.popleft())
+print(arr[0])
