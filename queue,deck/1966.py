f8d70e9250f786b67f7e92a95e83d926fcbe6cc1
bishoe01 <bishoe01@gmail.com>
2022-05-28 15:20:29 +0900

    completed solve 1966.py

--- queue,deck/1966.py
+++ queue,deck/1966.py
@@ -1,9 +1,20 @@
-from collections import deque
 n = int(input())
-deck = deque()
-priority = 0
-for _ in range(n):
+for i in range(n):
     num , target = map(int,input().split())
-    deck.append(map(int,input().split()))
-    while(True):
+    deck = list(map(int,input().split()))
+    answer = [0 for i in range(num)]
+    answer[target] = 1 #우선순위를 측정할 값
+    priority = 0  #최종 출력할 우선순위 변수
+    while True:
+        if(deck[0] == max(deck)):
+            priority += 1
+            if(answer[0] == 1):
+                print(priority)
+                break
+            else:
+                deck.pop(0)
+                answer.pop(0)
+        else:
+            deck.append(deck.pop(0))
+            answer.append(answer.pop(0))
         
