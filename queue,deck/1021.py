01a196978d659c31a2cf626c3e0f0ad226e034a2
bishoe01 <bishoe01@gmail.com>
2022-05-31 17:11:55 +0900

    5430.py solve, queue-deck end

--- queue,deck/1021.py
+++ queue,deck/1021.py
@@ -1,21 +1,19 @@
 from collections import deque
-n , m = map(int, input().split())
+n , m = map(int, input().split()) 
+target = list(map(int,input().split())) 
+arr = deque([i for i in range(1,n+1)]) #덱에 1부터 n까지 차례대로 증가하는 원소를 넣어줌
+cnt = 0  #정답 카운트
 
-target = list(map(int,input().split()))
-arr = deque([i for i in range(1,n+1)])
-cnt = 0 
-
-for element in target:
+for element in target : #찾아야하는 원소들을 리스트 반복문에 넣어줄 것이다. 
     while True:
         if(arr[0] == element):
-            arr.popleft()
+            arr.popleft() #찾으면 원소제거 후 다음 원소로 넘어가야함 
             break
         else:
-            if(arr.index(element) <= len(arr)//2):
-                arr.rotate(-1)
+            if(arr.index(element) <= len(arr)//2): #이부분이 핵심 찾을 원소가 절반기준으로 판단하여 뒤애서 앞으로 땡겨줄지, 뒤로 돌려줄지 판단
+                arr.rotate(-1) #arr.append(arr.pop())  
                 cnt += 1
             else:
-                arr.rotate(1)
+                arr.rotate(1)  #arr.appendleft(arr.popleft()) 
                 cnt+=1
-
 print(cnt)
---
+++ queue,deck/5430.py
@@ -0,0 +1,31 @@
+from collections import deque
+import sys
+n = int(sys.stdin.readline())
+for _ in range(n):
+    is_error = False
+    command = sys.stdin.readline().rstrip()
+    num_e = int(input())
+    arr = sys.stdin.readline().rstrip()[1:-1].split(",")
+    array =deque(arr)
+    cnt = 0
+    if(num_e ==0 ):
+        array = []
+    for element in command:
+        if(element == "R"): #ROTATE
+            cnt+=1
+        elif(element == "D"): #첫번쨰 버리기 
+            if(len(array) !=0):
+                if(cnt %2 == 0):
+                    array.popleft()
+                else:
+                    array.pop()
+            else:
+                is_error = True
+                print("error")
+                break
+    if(is_error==False):
+        if(cnt %2 == 0):
+            print("[" + ",".join(array) + "]")
+        else:
+            array.reverse()
+            print("[" + ",".join(array) + "]")
